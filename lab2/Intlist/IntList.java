a8b8cfeb8b62f1045649acddc13c8aadc4421872
mrmiywj <jsyangwenjie@gmail.com>
2017-02-07 01:18:32 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,7 +81,17 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null){
+            A = B;
+            return A;
+        }
+        if (A.rest == null) {
+            A.rest = B;
+            return A;
+        } else {
+            IntList tmp = dcatenate(A.rest, B);
+            return A;
+        }
     }
 
     /**
@@ -90,7 +100,14 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = new IntList();
+        if (A == null) {
+            return B;
+        } else {
+            res.first = A.first;
+            res.rest = catenate(A.rest, B);
+            return res;
+        }
     }
 
 
